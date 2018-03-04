20209bb2b2931a41088c66ea6195a79dac4db339
r-tamura <rtamura30@gmail.com>
2018-03-04 16:03:49 +0900

    [aes] add Phase shiftrows

---
+++ src/crypt/aes/shiftrows.js
@@ -0,0 +1,26 @@
+
+const shiftLeft = (xs, n) => {
+
+  if (n === 0) {
+    return [...xs]
+  }
+
+  const left  = xs.slice(0, n)
+  const right = xs.slice(n, xs.length)
+  return [...right, ...left]
+}
+
+/**
+ * AES Shift Rows処理
+ * 第i行目の要素をi-1列文左へシフトする
+ * 
+ * @param {Array} state
+ * @return {Array} Shift Rows
+ */
+const shiftRows = state => {
+  return state.map((row, i) => shiftLeft(row, i))
+}
+
+export {
+  shiftRows,
+}
--- src/crypt/aes/util/multiMap.js
+++ src/crypt/aes/util/multiMap.js
@@ -1,3 +1,12 @@
+/**
+ * Map多次元配列版
+ * 
+ * @param {Array} xs 多次元配列
+ * @param {Function} fn
+ * @param {number} dimension 次元数
+ * @param {Array} indexes 添え字
+ * @return {Array} 
+ */
 const multiMap = (xs, fn, dimension = Number.MAX_SAFE_INTEGER, ...indexes) => {
   if (dimension === 0 || !Array.isArray(xs)) {
     return fn(xs, ...indexes)
